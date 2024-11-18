92ed98e78d49c9e589a6bf7ac54b0a1ff8a47038
Quinten Krijger <quinten.krijger@ing.com>
2024-11-18 16:25:56 +0100

    ex B, part 3

--- exercise/b_functions/src/main.rs
+++ exercise/b_functions/src/main.rs
@@ -9,18 +9,13 @@ fn main() {
     let area = area_of(width, height);
     
     println!("Area is {}", area);
-
-    // 3. Uncomment the line below.  It doesn't work yet because the `volume` function doesn't exist.
-    //    Create the `volume` function!  It should:
-    //    - Take three arguments of type i32
-    //    - Multiply the three arguments together
-    //    - Return the result (which should be 280 when you run the program).
-    //
-    // If you get stuck, remember that this is *very* similar to what `area_of` does.
-    //
-    //println!("Volume is {}", volume(width, height, depth));
+    println!("Volume is {}", volume(width, height, depth));
 }
 
 fn area_of(x: i32, y: i32) -> i32 {
     x * y
+}
+
+fn volume(x: i32, y: i32, z: i32) -> i32 {
+    x * y * z
 }
