1e9ad8bd7d7b5ea5fd8722f79ec2cdadcc876c69
Quinten Krijger <quinten.krijger@ing.com>
2024-11-18 19:55:40 +0100

    ex C, part 5

--- exercise/c_simple_types/src/main.rs
+++ exercise/c_simple_types/src/main.rs
@@ -1,6 +1,8 @@
 // Silence some warnings so they don't distract from the exercise.
 #![allow(dead_code, unused_variables)]
 
+use ding_machine::{ding, print_array, print_difference, on_off};
+
 fn main() {
     let coords: (f32, f32) = (6.3, 15.0);
     print_difference(coords.0, coords.1);
@@ -14,50 +16,9 @@ fn main() {
     let mess = ([3, 2], 3.14, [(false, -3), (true, -100)], 5, "candy");
     on_off(mess.2[1].0);
 
-    // 5.  What a mess -- functions in a binary! Let's get organized!
-    //
-    // - Make a library file (src/lib.rs)
-    // - Move all the functions (except main) into the library
-    // - Make all the functions public with `pub`
-    // - Bring all the functions into scope using use statements. Remember, the name of the library
-    //   is defined in Cargo.toml.  You'll need to know that to `use` it.
-    //
-    // `cargo run` should produce the same output, only now the code is more organized. 🎉
-
     // Challenge: Uncomment the line below, run the code, and examine the
     // output. Then go refactor the print_distance() function according to the
     // instructions in the comments inside that function.
 
     // print_distance(coords);
 }
-
-fn print_difference(x: f32, y: f32) {
-    println!("Difference between {} and {} is {}", x, y, (x - y).abs());
-}
-
-fn print_array(a: [f32; 2]) {
-    println!("The coordinates are ({}, {})", a[0], a[1]);
-}
-
-fn ding(x: i32) {
-    if x == 13 {
-        println!("Ding, you found 13!");
-    }
-}
-
-fn on_off(val: bool) {
-    if val {
-        println!("Lights are on!");
-    }
-}
-
-fn print_distance(z: (f32, f32)) {
-    // Using z.0 and z.1 is not nearly as nice as using x and y.  Lucky for
-    // us, Rust supports destructuring function arguments.  Try replacing "z" in
-    // the parameter list above with "(x, y)" and then adjust the function
-    // body to use x and y.
-    println!(
-        "Distance to the origin is {}",
-        ( z.0.powf(2.0) + z.1.powf(2.0) ).sqrt());
-}
-
