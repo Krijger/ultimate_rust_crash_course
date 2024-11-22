8be18f2ed7e41a4ce187758e3fb3301aafac260f
Quinten Krijger <quinten.krijger@ing.com>
2024-11-22 10:50:25 +0100

    ex G, part 1

--- exercise/g_collections_enums/src/main.rs
+++ exercise/g_collections_enums/src/main.rs
@@ -1,23 +1,25 @@
 // Silence some warnings that could distract from the exercise
 #![allow(unused_variables, unused_mut, dead_code)]
 
-// Someone is shooting arrows at a target.  We need to classify the shots.
-//
-// 1a. Create an enum called `Shot` with variants:
-// - `Bullseye`
-// - `Hit`, containing the distance from the center (an f64)
-// - `Miss`
-//
-// You will need to complete 1b as well before you will be able to run this program successfully.
+enum Shot {
+    Bullseye,
+    Hit {distance: f64},
+    Miss,
+}
 
 impl Shot {
-    // Here is a method for the `Shot` enum you just defined.
     fn points(self) -> i32 {
-        // 1b. Implement this method to convert a Shot into points
-        // - return 5 points if `self` is a `Shot::Bullseye`
-        // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
-        // - return 1 point if `self` is a `Shot::Hit(x)` where x >= 3.0
-        // - return 0 points if `self` is a Miss
+        match self {
+            Self::Bullseye => 5,
+            Self::Miss => 0,
+            Self::Hit{distance: x} => {
+                if x < 3.0 {
+                    2
+                } else {
+                    1
+                }
+            },
+        }
     }
 }
 
