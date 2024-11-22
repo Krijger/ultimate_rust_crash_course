af869af34baf873c5f0b862a4d0db9a8d737cdd1
Quinten Krijger <quinten.krijger@ing.com>
2024-11-22 14:42:55 +0100

    ex H, clippy fix

--- exercise/h_closures_threads/src/main.rs
+++ exercise/h_closures_threads/src/main.rs
@@ -35,7 +35,7 @@ fn main() {
     let handle = thread::spawn(|| expensive_sum(my_vector));
 
     // While the child thread is running, the main thread will also do some work
-    for letter in vec!["a", "b", "c", "d", "e", "f"] {
+    for letter in ["a", "b", "c", "d", "e", "f"] {
         println!("Main thread: Letter {}", letter);
         pause_ms(200);
     }
